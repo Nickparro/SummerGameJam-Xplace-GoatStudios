e2056b78a2d377aa2d60d7ed1fefcb3ca9c9104e
Nicolas Parroquiano <nicolasparroquiano@gmail.com>
2024-06-14 22:12:40 -0500

    MainMenu and UI dded

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    //Intro
+    public GameObject mainCamera;
+    public CanvasGroup startMenuText, pressAnyButtonText, mainMenuText;
+    bool MainMenuScene(string sceneLoaded)
+    {
+        return SceneManager.GetActiveScene().name == sceneLoaded;
+    }
+    void Start()
+    {
+        if (MainMenuScene("MainMenu"))
+        {
+            MainMenuIntro_Cinematic();
+            startMenuText.alpha = 0;
+            pressAnyButtonText.alpha = 0;
+            mainMenuText.alpha = 0;
+        }
+    }
+
+    private void Update() {
+        if(Input.anyKeyDown)
+        {
+            AnyButtonToStartPressed();
+        }
+    }
+    public void MainMenuIntro_Cinematic()
+    {
+        var sequence = DOTween.Sequence();
+        
+        sequence.Insert(0f, mainCamera.transform.DORotate(new Vector3(-61.651f,38.783f,61.103f), 0f, RotateMode.Fast));
+        sequence.Insert(2f, mainCamera.transform.DORotate(new Vector3(-3.299f,64.132f,-1.045f), 7f, RotateMode.Fast).SetEase(Ease.InOutBack));
+        sequence.Insert(6f, startMenuText.DOFade(1f, 3f));
+        sequence.Insert(9f, pressAnyButtonText.DOFade(1f, 0.7f).SetLoops(-1, LoopType.Yoyo));
+    }
+
+    public void AnyButtonToStartPressed()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Insert(0f, startMenuText.DOFade(0f, 1f));
+        sequence.Insert(1f, mainMenuText.DOFade(1f, 1f));
+    }
+}
